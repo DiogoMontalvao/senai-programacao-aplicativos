d50014aa908d31863b1547c92c727f2f60886190
Diogo Montalvão <109600744+DiogoMontalvao@users.noreply.github.com>
2025-03-17 19:35:52 -0300

    Update

--- 4-diogo-montalvao-atividade-bicicleta/src/main/java/Main.java
+++ 4-diogo-montalvao-atividade-bicicleta/src/main/java/Main.java
@@ -1,22 +1,36 @@
 public class Main {
-
     public static void main(String[] args) {
+
         Acessorio paralama = new Acessorio("Paralama", "Do bão", "Plástico", 30.0);
-        Bicicleta monark = new Bicicleta("Monark", "Transporta Idoso", "Cargueira", 600.0, paralama);
+        Bicicleta monark = new Bicicleta(
+                "Monark",
+                "Transporta Idoso",
+                "Cargueira",
+                600.0,
+                paralama
+        );
 
         System.out.println(monark.exibirDetalhes());
         System.out.println("\n" + paralama.exibirDetalhes());
         System.out.printf(
                 "\nO valor total da bicicleta %s %s com o %s de %s ficou de: R$ %.2f\n",
-                monark.getMarca(), monark.getModelo(), paralama.getTipo(), paralama.getMaterial(), monark.calculaValorTotal());
+                monark.getMarca(), monark.getModelo(), paralama.getTipo(),
+                paralama.getMaterial(), monark.calculaValorTotal());
 
         Acessorio capacete = new Acessorio("Capacete", "GTA", "Plástico", 80.0);
-        Bicicleta caloi = new Bicicleta("Caloi", "Vulcan", "Mountain Bike", 1200.0, capacete);
+        Bicicleta caloi = new Bicicleta(
+                "Caloi",
+                "Vulcan",
+                "Mountain Bike",
+                1200.0,
+                capacete
+        );
 
         System.out.println("\n" + caloi.exibirDetalhes());
         System.out.println("\n" + capacete.exibirDetalhes());
         System.out.printf(
                 "\nO valor total da bicicleta %s %s com o %s de %s ficou de: R$ %.2f\n",
-                caloi.getMarca(), caloi.getModelo(), capacete.getTipo(), capacete.getMaterial(), caloi.calculaValorTotal());
+                caloi.getMarca(), caloi.getModelo(), capacete.getTipo(),
+                capacete.getMaterial(), caloi.calculaValorTotal());
     }
 }
