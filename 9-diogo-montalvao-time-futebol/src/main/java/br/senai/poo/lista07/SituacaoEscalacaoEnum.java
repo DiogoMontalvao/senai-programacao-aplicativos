001f337f3a6eb953f089076087c6557171ec3c53
Diogo Montalvão <109600744+DiogoMontalvao@users.noreply.github.com>
2025-04-14 15:05:31 -0300

    Update

--- 9-diogo-montalvao-time-futebol/src/main/java/br/senai/poo/lista07/SituacaoEscalacaoEnum.java
+++ 9-diogo-montalvao-time-futebol/src/main/java/br/senai/poo/lista07/SituacaoEscalacaoEnum.java
@@ -1,5 +1,15 @@
 package br.senai.poo.lista07;
 
 public enum SituacaoEscalacaoEnum {
-    TITULAR, RESERVA
+    TITULAR(11), RESERVA(12);
+
+    private final int quantidadeMax;
+
+    private SituacaoEscalacaoEnum(int quantidadeMax) {
+        this.quantidadeMax = quantidadeMax;
+    }
+
+    public int getQuantidadeMax() {
+        return this.quantidadeMax;
+    }
 }
